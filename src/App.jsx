166b65f8b5331b4138d06ad947a81bc24d05bb63
anupam-roy-swe <roya55324@gmail.com>
2024-03-17 13:07:47 +0600

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 // import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { FaBahai } from "react-icons/fa";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Ecommerce from "./pages/Ecommerce";
+
 const App = () => {
   const activeMenu = true;
   return (
@@ -19,10 +24,51 @@ const App = () => {
             {/* </TooltipComponent> */}
           </div>
           {activeMenu ? (
-            <dir className='w-72 fixed sidebar dark:bg-white'>sidebar</dir>
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white '>
+              <Sidebar />
+            </div>
           ) : (
-            <div>sidebar w-0</div>
+            <div className='w-0 dark:bg-secondary-dark-bg'>Sidebar</div>
           )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
+            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full '>
+              <Navbar />
+            </div>
+            {/* routes */}
+            <div>
+              {/* Dashboard */}
+              <Routes>
+                <Route path='/' element={<Ecommerce />} />
+                <Route path='/' element='ecomarce' />
+                {/* page */}
+
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                {/* app */}
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                {/* charts */}
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+                <Route path='/' element='ecomarce' />
+              </Routes>
+            </div>
+          </div>
         </div>
       </BrowserRouter>
     </div>
